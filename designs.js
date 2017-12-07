20c3c5e6e861eaae763e2362cf50bf0766ec090d
selmanbey <selmannur@yahoo.com>
2017-12-07 20:48:22 +0000

    finalized v1

--- designs.js
+++ designs.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
+
     var submitButton = $("#submit_button");
+    var mousepressed = false;
 
     function makeGrid() {
       var gridHeight = $("#input_height").val();
@@ -13,30 +15,54 @@ $(document).ready(function () {
       }
     };
 
-    /* DEFAULT CANVAS */
-    for (row = 0; row <= 30; row++) {
-      $("#pixel_canvas").append("<tr></tr>");
-        for (col = 0; col <= 30; col++) {
-          $("#pixel_canvas").first().append("<td></td>");
-        }
-    };
+    // DEFAULT CANVAS
+    if (window.matchMedia("(max-width: 438px)").matches) {
+      for (row = 0; row <= 15; row++) {
+        $("#pixel_canvas").append("<tr></tr>");
+          for (col = 0; col <= 15; col++) {
+            $("#pixel_canvas").first().append("<td></td>");
+          }
+      };
+    } else {
+      for (row = 0; row <= 22; row++) {
+        $("#pixel_canvas").append("<tr></tr>");
+          for (col = 0; col <= 22; col++) {
+            $("#pixel_canvas").first().append("<td></td>");
+          }
+      };
+    }
 
+    // RE-ARRANGING CANVAS
     submitButton.on("click", function(e) {
       e.preventDefault();
 
-      if ($("#input_height").val() <= 50 && $("#input_width").val() <= 50) {
+      if ($("#input_height").val() <= 30 && $("#input_width").val() <= 30 &&
+          $("#input_height").val() > 0 && $("#input_width").val() > 0) {
         $("table").find("*").remove();
         makeGrid();
-        // $("#sizePicker").remove();
-        // $("#gridHeader").remove();
       } else {
-        alert("The grid size should not exceed 50 cell in either directions!")
+        alert("The grid size should not be 0 or exceed 30 cell in either directions!")
       }
     });
 
+    // PAINTING ON CANVAS
+    $("table").delegate("td", "mousedown", function () {
+        mousepressed = true;
+        var color = $("#colorPicker").val();
+        if(mousepressed) {
+          $(this).css("background-color", color);
+        }
+      });
+
+    $("table").delegate("td", "mouseup", function () {
+        mousepressed = false;
+      });
 
     $("table").delegate("td", "mouseover", function () {
         var color = $("#colorPicker").val();
-        $(this).css("background-color", color)
+        if(mousepressed) {
+          $(this).css("background-color", color);
+        }
       });
+
 })
